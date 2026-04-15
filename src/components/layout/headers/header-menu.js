59574ebb8d0246fda81356f6516a55a1c5bdc5ca
Mouad-212 <mouadmeskini54@gmail.com>
2026-04-15 11:50:39 +0100

    synchronisation

--- src/components/layout/headers/header-menu.js
+++ src/components/layout/headers/header-menu.js
@@ -32,7 +32,7 @@ const MainMenu = () => {
                         {/*<li><Link to='/blog/key-steps-to-ensure-a-smooth-building-process'>Blog Details</Link></li>*/}
                     </ul>
                 </li>
-                <li className='menu-item-has-children'><Link to='/blog'>Équipements</Link>
+                {/* <li className='menu-item-has-children'><Link to='/blog'>Équipements</Link>
                     <ul className='sub-menu'>
                         <li><Link to='/blog'>Boiserie</Link></li>
                         <li><Link to='/blog-standard'>Plan de Travail</Link></li>
@@ -40,21 +40,21 @@ const MainMenu = () => {
                         <li><Link to='/blog-standard'>Poignées</Link></li>
                         <li><Link to='/blog-standard'>Quincailleries</Link></li>
                         <li><Link to='/blog-standard'>Éclairage intégré</Link></li>
-                        {/*<li><Link to='/blog/key-steps-to-ensure-a-smooth-building-process'>Blog Details</Link></li>*/}
+                        <li><Link to='/blog/key-steps-to-ensure-a-smooth-building-process'>Blog Details</Link></li>
                     </ul>
-                </li>
-                <li><Link to='/contact-us'>Témoignages</Link></li>  
+                </li> */}
+                <li><Link to='/testimonial'>Témoignages</Link></li>  
 
-                <li className='menu-item-has-children'><Link to='/blog'>Blog</Link>
+                <li className='menu-item-has-children'><Link to='/blog'>Équipements</Link>
                     <ul className='sub-menu'>
-                        <li><Link to="/equipment/bois">Bois</Link></li>
-                        <li><Link to="/equipment/planTravail">plan de travail</Link></li>
+                        <li><Link to="/equipment/bois">Boiserie</Link></li>
+                        <li><Link to="/equipment/planTravail">Plan de travail</Link></li>
                         <li><Link to="/equipment/poignee">POIGNÉES</Link></li>
                         <li><Link to="/equipment/accessoire">ACCESOIRES</Link></li>
                         <li><Link to="/equipment/quincaillerie">QUINCAILLERIE </Link></li>
-                        <li><Link to='/blog'>Blog Grid</Link></li>
-                        <li><Link to='/blog-standard'>Blog Standard</Link></li>
-                        <li><Link to='/blog/key-steps-to-ensure-a-smooth-building-process'>Blog Details</Link></li>
+                        <li><Link to='/blog'>Éclairage intégré</Link></li>
+                        {/* <li><Link to='/blog-standard'>Blog Standard</Link></li>
+                        <li><Link to='/blog/key-steps-to-ensure-a-smooth-building-process'>Blog Details</Link></li> */}
                     </ul>
                 </li>
                 <li><Link to='/contact-us'>Contact Us</Link></li>      
